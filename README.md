dc0bd01cf0f5511dcdcf172d1d9642516ce30f3b
lwu877 <lex@lexwu.com>
2025-12-26 15:32:22 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# LinkPage
+
+A page for me to share links with relevant images and descriptions in a convenient way.
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  theme: {
+    fontFamily: {
+      display: ['Unbounded Variable', 'sans-serif'],
+      sans: ['Figtree Variable', 'sans-serif'],
+    },
+  },
+}
